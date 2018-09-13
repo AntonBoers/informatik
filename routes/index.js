5495275a25cc8af455f502a0b806c8253c65c80e
Anton Martinussen <antonbm.drengen@gmail.com>
2018-09-13 18:25:16 +0200

    project

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+router.get('/databaser', function(req, res, next) {
+  res.render('databaser', { title: 'Express' });
+});
+
+
+router.get('/webdesign', function(req, res, next) {
+  res.render('webdesign', { title: 'Express' });
+});
+
+
+router.get('/quiz', function(req, res, next) {
+  res.render('quiz', { title: 'Express' });
+});
+
+router.get('/reklamespil', function(req, res, next) {
+  res.render('reklamespil', { title: 'Express' });
+});
+
+
+module.exports = router;
