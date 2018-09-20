8d4730e2efc1d903a40e021c8a10272bfde45421
Anton Martinussen <antonbm.drengen@gmail.com>
2018-09-20 23:48:57 +0200

    openode virker nu wop wop

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var particlesJS = require('particles.js');
 
 var app = express();
 
